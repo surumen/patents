a6df2c426ad2a5c62bdd8bd5719e4966f8f01f5c
Moses Surumen <moses.susrumen@gmail.com>
2019-03-05 22:22:48 -0800

    remove unwanted files

--- src/main/java/com/patents/patentapps/Application.java
+++ src/main/java/com/patents/patentapps/Application.java
@@ -1,7 +1,5 @@
 package com.patents.patentapps;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,8 +18,7 @@ import java.util.List;
 @SpringBootApplication
 public class Application {
 
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
-    static final String URL_PATENTS = "https://developer.uspto.gov/ibd-api/v1/patent/application?applicationNumber=US12645077";
+    private static final String URL_PATENTS = "https://developer.uspto.gov/ibd-api/v1/patent/application?applicationNumber=US12645077";
 
     public static void main(String args[]) {
         SpringApplication.run(Application.class);
@@ -48,20 +45,16 @@ public class Application {
 
             // Send request with GET method, and Headers.
             ResponseEntity<Root> response = restTemplate.exchange(URL_PATENTS, HttpMethod.GET, entity, Root.class);
-//            Root data = restTemplate.getForObject(
-//                    "https://developer.uspto.gov/ibd-api/v1/patent/application?applicationNumber=US12645077", Root.class);
-//            log.info(data.toString());
 
             Root result = response.getBody();
 
-            Response response1 = new Response();
-            response1 = result.getResponse();
+            Response response1 = result.getResponse();
 
             List<Patent> patents = response1.getDocs();
 
-            // Print the title from the list....
+            // Print the titles from the list of patents
             for(Patent patent : patents) {
-                System.out.println(patent.getTitle());
+                System.out.println(patent.getApplicant());
             }
         };
     }
