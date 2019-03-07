e2a945f682ac3d582b5592543db908aeeb96ddc4
Moses Surumen <moses.susrumen@gmail.com>
2019-03-07 01:34:04 -0800

    allow CORS

--- src/main/java/com/patents/patentapps/api/PatentRepository.java
+++ src/main/java/com/patents/patentapps/api/PatentRepository.java
@@ -1,8 +1,9 @@
 package com.patents.patentapps.api;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+@CrossOrigin
 public interface PatentRepository extends CrudRepository<Patent, Long> {
 
 }
--- src/main/java/com/patents/patentapps/controller/PatentController.java
+++ src/main/java/com/patents/patentapps/controller/PatentController.java
@@ -2,12 +2,13 @@ package com.patents.patentapps.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @Controller
 public class PatentController {
 
-
+    @CrossOrigin
     @RequestMapping(value = {"", "/", "/index"})
     public String index() {
         return "index";
