b1dd845041db8d0c9e686054609766c81f1b8bed
Moses Surumen <moses.susrumen@gmail.com>
2019-03-07 15:03:36 -0800

    CORS locally

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,16 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/main.css';
+import logo from'../assets/logo.svg';
 
 class Header extends Component {
-
   render() {
     return (
       <header className="bluebg" id="top">
         <div className="container-fluid">
           <nav className="row">
             <div className="col-xs-6 col-lg-12 logo">
-              <svg version={1.0} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 400 64" style={{enableBackground: 'new 0 0 400 64'}} xmlSpace="preserve">
-                <defs><style dangerouslySetInnerHTML={{__html: ".cls-1{font-size:60px;fill:#fff;font-family:GoogleSans-Bold, Google Sans;font-weight:700;}.cls-2{letter-spacing:-0.01em;}.cls-3{letter-spacing:-0.02em;}.cls-4{letter-spacing:-0.02em;}.cls-5{letter-spacing:0.01em;}.cls-6{letter-spacing:0em;}.cls-7{letter-spacing:-0.03em;}" }} /></defs><title>logo</title><text className="cls-1" transform="translate(0 51.48)">p<tspan className="cls-2" x="37.62" y={0}>a</tspan><tspan className="cls-3" x="70.74" y={0}>t</tspan><tspan x="94.8" y={0}>e</tspan><tspan className="cls-4" x="130.02" y={0}>n</tspan><tspan className="cls-5" x="164.52" y={0}>t</tspan><tspan className="cls-6" x="190.14" y={0}>se</tspan><tspan x="256.14" y={0}>a</tspan><tspan className="cls-7" x="289.8" y={0}>r</tspan><tspan x="313.68" y={0}>ch</tspan></text>
-              </svg>
+              <img height={64} width={200} src={logo} style={{enableBackground: 'new 0 0 400 64'}} alt="patentsearch" />
             </div>
             <div className="col-lg-8 visible-lg main-nav">
               <ul>
