63f2ef5caa5af3299988a5f1e299e85d6a3bc4f6
Moses Surumen <moses.susrumen@gmail.com>
2019-03-07 13:44:02 -0800

    Updates

---
+++ precache-manifest.e4cf9e315e2134f738fd4c859c3c73c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0119d0f424bc5712fdf",
+    "url": "/patents/static/js/runtime~main.e86a3c26.js"
+  },
+  {
+    "revision": "0dcf46a8733f50e09fd8",
+    "url": "/patents/static/js/main.dae2760a.chunk.js"
+  },
+  {
+    "revision": "27faa41e2d372d5a7731",
+    "url": "/patents/static/js/2.33d1e660.chunk.js"
+  },
+  {
+    "revision": "0dcf46a8733f50e09fd8",
+    "url": "/patents/static/css/main.5fab6246.chunk.css"
+  },
+  {
+    "revision": "d9126638ec4d63de9ffc72b10ba3a875",
+    "url": "/patents/index.html"
+  }
+];
