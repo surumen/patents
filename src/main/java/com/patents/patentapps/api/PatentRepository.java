6bdb6b5a4c51d3989d6ac85c0f8575de162e93f0
Moses Surumen <moses.susrumen@gmail.com>
2019-03-07 18:56:07 -0800

    CORS

--- src/main/java/com/patents/patentapps/api/PatentRepository.java
+++ src/main/java/com/patents/patentapps/api/PatentRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@CrossOrigin(origins = "https://github.com/surumen/patents",
+@CrossOrigin(origins = "https://surumen.github.io/patents",
         methods = { RequestMethod.GET },
         maxAge = 3600)
 public interface PatentRepository extends CrudRepository<Patent, Long> {
